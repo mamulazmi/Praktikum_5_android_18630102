177cb0038aef34d59d9fd50ded0b419b5ca11eb1
Imamul Azmi <imamul.azmi48@gmail.com>
2020-10-31 19:54:42 +0800

    Initial commit

---
+++ app/src/main/java/com/mamulazmi/praktikum518630102/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mamulazmi.praktikum518630102;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
